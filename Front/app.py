ddb46470ab7fdeb057aee70768526a34bf370ffe
쟈에요 <105041123+namu1029@users.noreply.github.com>
2024-05-02 14:34:05 +0900

    프론트 작업

--- Front/app.py
+++ Front/app.py
@@ -2,8 +2,12 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        target_ip = request.form.get('ip')
+        #results = sacn('ip')
+        return render_template("results.html", target_ip=target_ip)
     return render_template("index.html")
 
 if __name__ == '__main__':
