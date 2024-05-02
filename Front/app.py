9b1ed99b2afcd3714a7fb0530efcd638981888f6
쟈에요 <105041123+namu1029@users.noreply.github.com>
2024-05-02 16:53:19 +0900

    프론트 작업

--- Front/app.py
+++ Front/app.py
@@ -2,13 +2,20 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
+    return render_template("index.html")
+
+@app.route('/', methods=['GET', 'POST'])
+def results():
     if request.method == 'POST':
         target_ip = request.form.get('ip')
         #results = sacn('ip')
         return render_template("results.html", target_ip=target_ip)
-    return render_template("index.html")
+    
+@app.route('/vulner')
+def vulner():
+    return render_template("vulner.html")
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
